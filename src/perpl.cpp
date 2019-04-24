369a4df4296ce0425621826e3b79525f4d69a66b
Кузьмин Матвей Александрович <ip815s07@lwp09.csc.local>
2019-04-24 13:09:57 +0700

    Update perpl.cpp

--- src/perpl.cpp
+++ src/perpl.cpp
@@ -1,8 +1,26 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#
+#define pi = 3, 14
 int main()
 {
+    struct circle {
+        int x;
+        int y;
+        int r;
+    } cir;
+    float S;
+    int k;
+    while (1) {
+        scanf("%d %d %d", &cir.x, &cir.y, cir.r);
+        S = pi * cir.r;
+        printf("%f ", S);
+        printf("End?");
+        if (kbhit())
+            k = getch;
+        if (k = 27)
+            break;
+    }
+
     return 0;
 }
