fbbe0669c23a3d16bf1e0afcf28030fee50fe53e
Кузьмин Матвей Александрович <ip815s07@lwp09.csc.local>
2019-05-08 13:15:31 +0700

    Fix .cpp

--- src/geom.cpp
+++ src/geom.cpp
@@ -10,27 +10,27 @@ const int n = 3;
 
 int main()
 {
-    int i = 0, p = 0, k = 0, it[n][9], er=0, n1=0;
+    int i = 0, p = 0, k = 0, it[n][9], er = 0, n1 = 0;
     char A[] = "circle(", B[] = "triangle(", s1[80], s2[80] = {0}, emp[] = "\0",
          exit[] = "exit";
 
-    while (n1!=n) {
-    	printf("%d. ",n1+1);
+    while (n1 != n) {
+        printf("%d. ", n1 + 1);
         scanf("%s", s1);
         if (strcmp(exit, s1) == 0)
             break;
         if (strncmp(A, s1, 7) == 0) {
-        	it[n1][8]=1;
-        	er=1;
+            it[n1][8] = 1;
+            er = 1;
             i = 7;
             p = 0;
             k = 7;
             while (p < 2) {
-		if (s1[i] == ')'){
-		it[n1][8]=4;
-		p=10;
-		continue;		
-		}
+                if (s1[i] == ')') {
+                    it[n1][8] = 4;
+                    p = 10;
+                    continue;
+                }
                 if (s1[i] == ',') {
                     it[n1][p] = atoi(s2);
                     strcpy(s2, emp);
@@ -44,10 +44,10 @@ int main()
                 i++;
             }
             while (p != 0) {
-		if (p == 10){
-		p=0;
-		continue;		
-		}
+                if (p == 10) {
+                    p = 0;
+                    continue;
+                }
                 if (s1[i] == ')') {
                     it[n1][2] = atof(s2);
                     strcpy(s2, emp);
@@ -63,17 +63,17 @@ int main()
         }
 
         if (strncmp(B, s1, 9) == 0) {
-        	it[n1][8]=2;
-			er=1;
+            it[n1][8] = 2;
+            er = 1;
             i = 9;
             p = 0;
             k = 9;
             while (p < 7) {
-		if (s1[i] == ')'){
-			it[n1][8]=4;
-			p=10;
-			continue;		
-		}
+                if (s1[i] == ')') {
+                    it[n1][8] = 4;
+                    p = 10;
+                    continue;
+                }
                 if (s1[i] == ',') {
                     it[n1][p] = atoi(s2);
                     strcpy(s2, emp);
@@ -86,10 +86,10 @@ int main()
                 i++;
             }
             while (p != 0) {
-		if (p == 10){
-		p=0;
-		continue;		
-		}
+                if (p == 10) {
+                    p = 0;
+                    continue;
+                }
                 if (s1[i] == ')') {
                     it[n1][p] = atoi(s2);
                     strcpy(s2, emp);
@@ -103,14 +103,13 @@ int main()
                 i++;
             }
         }
-	if(er==0){
-        	it[n1][8]=3;
-		}
-	er=0;
+        if (er == 0) {
+            it[n1][8] = 3;
+        }
+        er = 0;
         n1++;
-        
     }
-	per(it);
+    per(it);
 
     return 0;
 }
